b0afc4364841bb60a9781982f99050af70e6b2ea
tpetry <github@tpetry.me>
2025-09-10 11:14:36 +0200

    full arktype testing

--- cases/arktype.ts
+++ cases/arktype.ts
@@ -1,7 +1,7 @@
 import { type } from 'arktype';
-import { createCase } from '../benchmarks';
+import { addCase } from '../benchmarks';
 
-const t = type({
+const LooseType = type({
   number: 'number',
   negNumber: 'number',
   maxNumber: 'number',
@@ -15,9 +15,52 @@ const t = type({
   },
 });
 
-createCase('arktype', 'assertLoose', () => {
-  return data => {
-    if (t.allows(data)) return true;
-    throw new Error('Invalid');
-  };
+const SafeType = type({
+  '+': 'delete',
+  number: 'number',
+  negNumber: 'number',
+  maxNumber: 'number',
+  string: 'string',
+  longString: 'string',
+  boolean: 'boolean',
+  deeplyNested: {
+    '+': 'delete',
+    foo: 'string',
+    num: 'number',
+    bool: 'boolean',
+  },
+}).onDeepUndeclaredKey('delete');
+
+const StrictType = type({
+  '+': 'reject',
+  number: 'number',
+  negNumber: 'number',
+  maxNumber: 'number',
+  string: 'string',
+  longString: 'string',
+  boolean: 'boolean',
+  deeplyNested: {
+    '+': 'reject',
+    foo: 'string',
+    num: 'number',
+    bool: 'boolean',
+  },
+});
+
+addCase('arktype', 'assertLoose', data => {
+  if (LooseType.allows(data)) return true;
+  throw new Error('Invalid');
+});
+
+addCase('arktype', 'assertStrict', data => {
+  if (StrictType.allows(data)) return true;
+  throw new Error('Invalid');
+});
+
+addCase('arktype', 'parseSafe', data => {
+  return SafeType.assert(data);
+});
+
+addCase('arktype', 'parseStrict', data => {
+  return StrictType.assert(data);
 });
